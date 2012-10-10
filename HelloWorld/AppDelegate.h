936fee88f9997512cca121ac41d1902ffbe4a75f
shinderuman <shinderuman@gmail.com>
2012-10-10 21:18:31 +0900

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by shinderuman on 2012/10/10.
+//  Copyright (c) 2012年 shinderuman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by shinderuman on 2012/10/10.
+//  Copyright (c) 2012年 shinderuman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
