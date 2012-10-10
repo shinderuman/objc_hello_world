f33d3a268cb558faaa0623c2bce3258d097d0b64
shinderuman <shinderuman@gmail.com>
2012-10-10 21:25:32 +0900

    add btn change event

--- HelloWorld/ViewController.h
+++ HelloWorld/ViewController.h
@@ -8,6 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    IBOutlet UIButton *button1;
+    IBOutlet UILabel *label1;
+}
+
+- (IBAction)button1OnDown:(id)sender;
 
 @end
